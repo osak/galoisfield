5075fec210dec46f73903a108bb9d75814e4835c
Koga Osamu <osak.63@gmail.com>
2013-09-10 01:56:53 +0900

    Add some tests

--- spec/galoisfield_spec.rb
+++ spec/galoisfield_spec.rb
@@ -6,5 +6,37 @@ describe GaloisField do
   it "should return Class" do
     expect(GaloisField[42]).to be_instance_of(Class)
   end
+
+  it "should return same class for same modulo" do
+    expect(GaloisField[42]).to be_equal(GaloisField[42])
+  end
+
+  context "Arithmetic expression" do
+    before do
+      @a = 3.to_galois(5)
+      @b = 4.to_galois(5)
+    end
+
+    it {expect(@a + @b).to eq(2.to_galois(5))}
+    it {expect(@a - @b).to eq(4.to_galois(5))}
+    it {expect(@a * @b).to eq(2.to_galois(5))}
+    it {expect(@a / @b).to eq(2.to_galois(5))}
+    it {expect(@a.inv).to eq(2.to_galois(5))}
+  end
+
+  it "should be hashed" do
+    hash = {}
+    val = 42.to_galois(10)
+    hash[val] = 42
+    expect(hash[val]).to eq(42)
+  end
+end
+
+describe Integer do
+  it "should be converted into galois number" do
+    a = 42.to_galois(10)
+    expect(a).to be_instance_of(GaloisField[10])
+    expect(a.to_i).to eq(2)
+  end
 end
 
